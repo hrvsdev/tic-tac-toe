d6ff4469d60af08598008f28aa45ae5b27ca1242
kingvyas <kumarvyasss@gmail.com>
2022-09-18 15:15:08 +0530

    added board ui

---
+++ src/lib/Board.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let turn: "O" | "X" = "X";
+  
+</script>
+
+<div class="container">
+  <div class="cell">X</div>
+  <div class="cell">O</div>
+  <div class="cell">X</div>
+  <div class="cell">O</div>
+  <div class="cell">X</div>
+  <div class="cell">O</div>
+  <div class="cell">X</div>
+  <div class="cell">O</div>
+  <div class="cell">X</div>
+</div>
+
+<style>
+  .container {
+    width: 454px;
+    height: 454px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-gap: 4px;
+    background-color: black;
+  }
+
+  .cell {
+    display: flex;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    font-size: 110px;
+    background-color: white;
+  }
+</style>
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,10 +1,17 @@
-<script lang="ts">
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
+<script>
+  import Board from "./Board.svelte";
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<main>
+  <Board />
+</main>
+
+<style>
+  main {
+    display: flex;
+    width: 100vw;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
