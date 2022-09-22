50077d7975bbdace93a9224f1362823a0d295f41
kingvyas <kumarvyasss@gmail.com>
2022-09-22 13:13:46 +0530

    making code better

--- src/lib/CreateGame.svelte
+++ src/lib/CreateGame.svelte
@@ -6,16 +6,21 @@
 
   import type { IGame } from "../firebase/types";
 
+  // Create a new game function
   const createGame = () => {
+    // Setting player to host and making it connected
     $data.host.isDisconnected = false;
     $player = "X";
 
+    // Creating a new game and getting its uid
     const res = newGame($data);
-    id.set(res.id);
-    console.log(res.id);
+    id.set(res.id), alert(res.id);
+
+    // Showing the board
     show.set(true);
   };
 
+  // Join a game
   const openGame = async () => {
     const path = window.location.pathname.substring(1);
     id.set(path);
@@ -26,7 +31,7 @@
     if (data.host.isDisconnected) return alert("Host is disconnected");
     if (!data.friend.isDisconnected) return alert("There are already connections");
 
-    updateGame(path, { friend: { isDisconnected: false,  } });
+    updateGame(path, { friend: { isDisconnected: false } });
     $player = "O";
     show.set(true);
   };
