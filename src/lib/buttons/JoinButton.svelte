b93c3864c303da9827488d136221aef305ceeda7
kingvyas <kumarvyasss@gmail.com>
2022-09-23 22:04:11 +0530

    added click handler in both buttons

--- src/lib/buttons/JoinButton.svelte
+++ src/lib/buttons/JoinButton.svelte
@@ -1,3 +1,41 @@
-<script></script>
+<script lang="ts">
+  import { get, ref } from "firebase/database";
 
-<button>Join game</button>
+  import { player, id } from "../game/Board.svelte";
+  import { show } from "../Home.svelte";
+  import { db, updateGame } from "../../firebase/db";
+
+  import type { IGame } from "../../firebase/types";
+
+  export let queryId: string;
+
+  // Join a game function
+  const openGame = async () => {
+    // Setting id
+    id.set(queryId);
+
+    // Getting snapshot from db via id
+    const snap = await get(ref(db, queryId));
+
+    // Check if game exists
+    if (snap.exists() === false) return alert("Game doesn't exists");
+
+    // Getting data from snap
+    const data = snap.val() as IGame;
+
+    // Checking if host is connected and friend is disconnceted
+    if (data.host.isDisconnected) return alert("Host is disconnected");
+    if (!data.friend.isDisconnected) return alert("There are already connections");
+
+    //  Updating db with friend as online
+    updateGame(queryId, { friend: { isDisconnected: false, sign: "O" } });
+
+    // Setting player
+    player.set("O");
+
+    // Showing the board
+    show.set(true);
+  };
+</script>
+
+<button on:click={openGame}>Join game</button>
--- src/lib/join-game/JoinGame.svelte
+++ src/lib/join-game/JoinGame.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import queryString from "query-string";
+
   import CreateButton from "../buttons/CreateButton.svelte";
   import JoinButton from "../buttons/JoinButton.svelte";
-  
+
+  // Getting game id from URL
+  const queryId = queryString.parse(window.location.search).g as string;
 </script>
 
 <h1>Create a new game</h1>
-<CreateButton/>
+<CreateButton />
 {#if queryId}
-  <JoinButton on:click={openGame} />
+  <JoinButton {queryId} />
 {/if}
