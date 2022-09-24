53f62bf1470248c5e4777b5f22683202f1ce9cbc
kingvyas <kumarvyasss@gmail.com>
2022-09-24 14:46:17 +0530

    changed the colors

--- src/lib/into-game/JoinGame.svelte
+++ src/lib/into-game/JoinGame.svelte
@@ -1,48 +1,48 @@
 <div class="card-wrapper">
-    <h3>Create a new game</h3>
-    <p>Create a new game and invite your friends to play or only spectate.</p>
-    <button>Create</button>
-  </div>
-  
-  <style>
-    .card-wrapper {
-      outline: 2px solid #17c964;
-      background-color: #f1fdf7;
-      box-shadow: rgb(136, 241, 182) 0px 4px 14px 0px;
-      margin-bottom: 20px;
-      border-radius: 15px;
-      overflow: hidden;
-      max-width: 400px;
-    }
-  
-    h3 {
-      padding: 12px 20px;
-      font-size: 19px;
-      font-weight: 500;
-    }
-  
-    p {
-      border-top: 2px solid #17c964;
-      padding: 25px 20px;
-      font-size: 17px;
-      color: rgba(0 0 0/0.8)  }
-  
-    button {
-      all: unset;
-      width: 100%;
-      cursor: pointer;
-      box-sizing: border-box;
-      font-size: 17px;
-      font-weight: 500;
-      padding: 12px 20px;
-      text-align: center;
-      color: white;
-      background-color: #17c964;
-      transition: all 300ms;
-    }
-  
-    button:hover {
-      filter: brightness(0.95);
-    }
-  </style>
-  
+  <h3>Create a new game</h3>
+  <p>Create a new game and invite your friends to play or only spectate.</p>
+  <button>Create</button>
+</div>
+
+<style>
+  .card-wrapper {
+    outline: 2px solid #ff4ecd;
+    background-color: #fff0fb;
+    box-shadow: #ff7ad9 0px 4px 14px 0px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    overflow: hidden;
+    max-width: 400px;
+  }
+
+  h3 {
+    padding: 12px 20px;
+    font-size: 19px;
+    font-weight: 500;
+  }
+
+  p {
+    border-top: 2px solid #ff4ecd;
+    padding: 25px 20px;
+    font-size: 17px;
+    color: rgba(0 0 0/0.8);
+  }
+
+  button {
+    all: unset;
+    width: 100%;
+    cursor: pointer;
+    box-sizing: border-box;
+    font-size: 17px;
+    font-weight: 500;
+    padding: 12px 20px;
+    text-align: center;
+    color: white;
+    background-color: #ff4ecd;
+    transition: all 300ms;
+  }
+
+  button:hover {
+    filter: brightness(0.95);
+  }
+</style>
