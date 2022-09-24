efb7e70093ecea10145a5fe82ff7beadc24e00be
kingvyas <kumarvyasss@gmail.com>
2022-09-24 09:54:50 +0530

    adding card

--- src/lib/into-game/IntoGame.svelte
+++ src/lib/into-game/IntoGame.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import CreateButton from "../buttons/CreateButton.svelte";
   import JoinButton from "../buttons/JoinButton.svelte";
+  import JoinGame from "./JoinGame.svelte";
 
   import { getQueryId } from "../utils";
 
   const queryId = getQueryId();
 </script>
 
-<h1>Create a new game</h1>
-<CreateButton />
-{#if queryId}
-  <JoinButton/>
-{/if}
+<JoinGame/>
---
+++ src/lib/into-game/JoinGame.svelte
@@ -0,0 +1,36 @@
+<div class="card-root">
+  <h3>Create a new game</h3>
+  <p>Create a new game and invite your friends to play or only spectate.</p>
+  <div class="bottom"><button>Create</button></div>
+</div>
+
+<style>
+  .card-root {
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+    max-width: 400px;
+  }
+
+  h3 {
+    padding: 15px 20px;
+    font-size: 19px;
+    font-weight: 500;
+  }
+
+  p {
+    padding: 15px 20px;
+    font-size: 17px;
+    border-top: 1px solid rgb(0 0 0/0.15);
+    border-bottom: 1px solid rgb(0 0 0/0.15);
+  }
+
+  button{
+    all: unset;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 15px 20px;
+    text-align: center;
+    border-radius: 0 0 15px 15px;
+    background-color: #17C964;
+  }
+</style>
