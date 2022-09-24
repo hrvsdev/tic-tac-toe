a0489f4c9a9a2be20529eeca99be6eb1fac37945
kingvyas <kumarvyasss@gmail.com>
2022-09-24 14:59:57 +0530

    removed unnecessary files

--- src/lib/buttons/CreateButton.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import createGame from "../commands/createGame";
-</script>
-
-<button on:click={createGame}>Create new game</button>
--- src/lib/buttons/JoinButton.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import joinGame from "../commands/joinGame";
-</script>
-
-<button on:click={joinGame}>Join game</button>
