fcbc21115ec2f7a65e7d438aa22bde5df4a0bb36
kingvyas <kumarvyasss@gmail.com>
2022-09-27 21:09:29 +0530

    adding hero

---
+++ src/lib/into-game/Hero.svelte
@@ -0,0 +1,7 @@
+<h1>Tic Tac Toe</h1>
+
+<style>
+  h1 {
+    font-size: 100px;
+  }
+</style>
--- src/lib/into-game/IntoGame.svelte
+++ src/lib/into-game/IntoGame.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  import Hero from "./Hero.svelte";
   import CreateGame from "./CreateGame.svelte";
   import JoinGame from "./JoinGame.svelte";
   import CreateGameInfo from "./CreateGameInfo.svelte";
   import JoinGameInfo from "./JoinGameInfo.svelte";
 </script>
 
+<Hero/>
 <section>
-  <CreateGameInfo/>
-  <JoinGameInfo/>
+  <CreateGame/>
+  <JoinGame/>
 </section>
 
 <style>
