b99c469d449a7d6a5713837f25dcfd9791aebb50
kingvyas <kumarvyasss@gmail.com>
2022-09-27 21:21:26 +0530

    changed heading to different element

--- src/lib/into-game/Hero.svelte
+++ src/lib/into-game/Hero.svelte
@@ -1,8 +1,17 @@
-<h1>Tic Tac Toe</h1>
+<section>
+  <h1>Tic&nbsp</h1>
+  <h1>Tac&nbsp</h1>
+  <h1>Toe</h1>
+</section>
 
 <style>
   h1 {
     font-size: 100px;
     font-family: "Pacifico";
+    display: inline;
+  }
+
+  h1:first-child{
+    color: var(--green-text);
   }
 </style>
