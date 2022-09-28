0d6e7d28dc42e6834e0439a2f85bb2e4fbafdad1
kingvyas <kumarvyasss@gmail.com>
2022-09-28 10:04:01 +0530

    replaced query id with hash id

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,6 +11,6 @@ export const winLogic = [
   [2, 4, 6],
 ];
 
-export const getQueryId = () => window.location.hash.substring(1)
+export const getURLId = () => window.location.hash.substring(1)
 
 // export const getQueryId = () => queryString.parse(window.location.search).g as string;
