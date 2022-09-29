1be49ba82be171f7e612396f262b11454b239210
kingvyas <kumarvyasss@gmail.com>
2022-09-29 09:27:11 +0530

    importing svg with plugon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelteSVG } from "rollup-plugin-svelte-svg";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelteSVG({
+      svgo: {},
+      enforce: "pre",
+    }),
+    svelte(),
+  ],
+});
