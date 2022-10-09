4ecc751736f37c8c2baeed3d7f58ec7feda05623
kingvyas <kumarvyasss@gmail.com>
2022-10-09 10:00:17 +0530

    trying to fix disconnection errors

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
+	import { onDisconnect, onValue, ref } from 'firebase/database';
+	import { db } from '../firebase/db';
+	import { player, id, data } from '../stores/game-store';
+
 	import Header from './into-game/header/Header.svelte';
+
+	import type { IGame } from '../firebase/types';
+
+	// Getting realtime data by firebase snapshot
+	if ($id) {
+		onValue(ref(db, $id), (res) => {
+			if (res.exists()) {
+				$data = res.val() as IGame;
+			} else {
+				$data.host.isDisconnected = true;
+			}
+		});
+	}
+
+	// Removing if host disconnects
+	if ($player === 'X' && $id) onDisconnect(ref(db, $id)).remove();
+
+	// Setting disconnection to true if friend disconnects
+	if ($player === 'O' && $id) onDisconnect(ref(db, $id)).update({ 'friend/isDisconnected': true });
 </script>
 
 <main>
