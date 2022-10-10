f1854eec9b4318580b7413ed6176cb34ce6af41e
kingvyas <kumarvyasss@gmail.com>
2022-10-10 14:08:01 +0530

    fixed a bug with layout in id url join comp

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,14 @@
 <script module="context" lang="ts">
+	import AlreadyGame from '$lib/into-game/already-game/AlreadyGame.svelte';
 	import JoinGameInfo from '$lib/into-game/join-game/JoinGameInfo.svelte';
 	import Layout from '$lib/Layout.svelte';
 </script>
 
 <Layout>
-	<JoinGameInfo />
+	<svelte:fragment slot="game">
+		<AlreadyGame />
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<JoinGameInfo />
+	</svelte:fragment>
 </Layout>
