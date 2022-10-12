a61853438223fbb4c6819375170b89cff80efd9a
kingvyas <kumarvyasss@gmail.com>
2022-10-12 23:28:55 +0530

    styling modal

--- src/lib/game/modal/Modal.svelte
+++ src/lib/game/modal/Modal.svelte
@@ -1,3 +1,13 @@
-<div class="modal-wrapper">
+<div class="wrapper">
     <div class="modal"></div>
 </div>
+
+<style>
+    .wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
