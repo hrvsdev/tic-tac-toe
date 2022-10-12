92bd39ce2a9d763d430d0b981a45e7f1bbd0aa67
kingvyas <kumarvyasss@gmail.com>
2022-10-12 23:29:36 +0530

    added black overlay in background of modal

--- src/lib/game/modal/Modal.svelte
+++ src/lib/game/modal/Modal.svelte
@@ -1,13 +1,14 @@
 <div class="wrapper">
-    <div class="modal"></div>
+	<div class="modal" />
 </div>
 
 <style>
-    .wrapper {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
+	.wrapper {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgb(0 0 0/0.2);
+	}
 </style>
